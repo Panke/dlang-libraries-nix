5f19dde72de3fa4e6eb5cd153c0738938d2f09cf
Tobias Pankrath <tobias.pankrath@vattenfall.de>
2022-04-21 15:39:15 +0000

    mir-ion: adjust sha256

--- mir/ion/default.nix
+++ mir/ion/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation
     src = fetchgit {
         url = "https://github.com/libmir/mir-ion";
         rev = "ec28d7e51d859896ef8ef923db6da5f3ed97bf9d";
-        sha256 = "1b6fv47aich23z0amhipx03wgpzh4jn1j3wlngm9px68c38n1cmh";
+        sha256 = "/r8o4qPLWGlECMp8GD0jmi1hr3t+IqVqrw7KnsKMU4c=";
         name = "mir-ion";
     };
 
