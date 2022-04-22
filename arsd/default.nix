f2f1c234d3f61261217dc5e8409eca7a86641178
Tobias Pankrath <tobias.pankrath@vattenfall.de>
2022-04-22 11:20:11 +0000

    arsd: add arsd built with meson

---
+++ arsd/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, meson, ninja, pkg-config, ldc, fetchgit,
+}:
+stdenv.mkDerivation
+{
+    name = "arsd";
+    version = "10.8.2";
+    srcs = [ 
+        (fetchgit {
+            url = "https://github.com/adamdruppe/arsd";
+            rev = "79b630e5474e42f44a412a3502d9496df359da73";
+            sha256 = "g78uwUxZ2Uio0ocNpwZj5h7kWHhcIcU4xcWLytGOnnc=";
+            name = "arsd";
+        })
+        ./package-files
+    ];
+    sourceRoot = "arsd";
+
+    mesonBuildType="debugoptimized";
+    mesonFlags="-Ddefault_library=both";
+
+    nativeBuildInputs = [ meson ninja ldc pkg-config];
+
+    prePatch = ''
+        cp ../package-files/* .
+    '';
+}
--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Packaged libraries for the D programming language";
-  outputs = { self, nixpkgs }: 
-  let 
+  outputs = { self, nixpkgs }:
+  let
     overlay_ = self: super: {
       deimos-zeromq = self.callPackage ./deimos/zeromq {};
       zmqd = self.callPackage ./zmqd {};
@@ -9,12 +9,13 @@
       mir-cpuid = self.callPackage ./mir/cpuid {};
       mir-algorithm = self.callPackage ./mir/algorithm {};
       mir-ion = self.callPackage ./mir/ion {};
+      arsd = self.callPackage ./arsd {};
     };
     pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ overlay_ ]; };
   in
   rec {
     overlay.x86_64-linux = overlay_;
-    packages.x86_64-linux = 
+    packages.x86_64-linux =
     {
       deimos-zeromq = pkgs.deimos-zeromq;
       zmqd = pkgs.zmqd;
@@ -22,6 +23,7 @@
       mir-cpuid = pkgs.mir-cpuid;
       mir-algorithm = pkgs.mir-algorithm;
       mir-ion = pkgs.mir-ion;
+      arsd = pkgs.arsd;
     };
   };
 }
