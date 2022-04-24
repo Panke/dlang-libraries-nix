66d02629586feba43f8b2455408689f159da378a
Tobias Pankrath <tobias.pankrath@vattenfall.de>
2022-04-24 15:11:35 +0000

    fixedpoint: update to include bugfix

--- fixedpoint/default.nix
+++ fixedpoint/default.nix
@@ -6,9 +6,9 @@ stdenv.mkDerivation
     version = "0.1.3";
     srcs = [ 
         (fetchgit {
-            url = "https://github.com/m3m0ry/fixedpoint.git";
-            rev = "205189404f86c6f6f3f78a32cc4e78dc88732589";
-            sha256 = "WhJup8xxr4QHA2DfphZFXmaT6BeW1LM3UV1EwDeMtcE=";
+            url = "https://github.com/panke/fixedpoint.git";
+            rev = "763cdfcee36f933d62e65140225890464b9a4d56";
+            sha256 = "fEGe9WntJg6b+b6FAswKaGxb3elfoeVdtCmWR9IK5I4=";
             name = "fixedpoint";
         })
         ./package-files
